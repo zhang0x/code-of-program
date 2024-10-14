ceb6ba27e683a9a0f40bbd3cc66e204e3764935d
zhanglinchen <3489588431@qq.com>
2024-10-14 17:02:32 +0800

    check first commit

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    cout<<"hello world\n";
+    return;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0),cout.tie(0);
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+    system("pause");
+    return 0;
+}
